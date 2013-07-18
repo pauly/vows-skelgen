fc908f8f0b971591737e9a0344cac795fac60ab0
pauly <pauly@clarkeology.com>
2013-07-18 11:56:22 +0000

    shebang!

--- skelgen.js
+++ skelgen.js
@@ -1,9 +1,3 @@
-/**
- * Vows skeleton test generator
- * https://github.com/pauly/vows-skelgen
- * 
- * @author PC <paulypopex@gmail.com>
- */
-
+#!/usr/bin/env node
 var skelgen = require( './lib/skelgen' );
 console.log( skelgen.generate( ));
