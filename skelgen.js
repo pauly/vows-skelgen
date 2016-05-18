0c9dbc93e0a7c61f17d1096fade0477ace50294a
Paul Clarke <paul.clarke@holidayextras.com>
2016-05-18 14:03:20 +0100

    create sample tests again

--- skelgen.js
+++ skelgen.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 'use strict';
 var skelgen = require('./lib/skelgen');
-skelgen.generate();
+var args = require('minimist')(process.argv.slice(2));
+skelgen.generate(args);
 process.exit(0); // eslint-disable-line no-process-exit
